1cd291a5dec952fab126c8642492e013b87c11e3
nasil95 <sktlf58@gmail.com>
2026-02-26 16:49:01 +0900

    third commit

---
+++ js/carousel.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    var owl = $('.new_contents');
+    owl.owlCarousel({
+        margin: 20,
+        loop: true,
+        items: 3.5
+    })
+
+    var owl = $('.kids_contents');
+    owl.owlCarousel({
+        margin: 38,
+        dots: false,
+        loop: true,
+        items: 2.7
+    })
+
+
+});
